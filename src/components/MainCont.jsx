96a8276d305eb9a58366122fdd5115895f4c9a0d
VaheAA <vahe.abov@gmail.com>
2022-05-03 13:59:07 +0400

    delete unnecesary imports

--- src/components/MainCont.jsx
+++ src/components/MainCont.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Collaboration from './Collaboration';
-import Collection from './Collection';
 import CTA from './CTA';
 import Intelligence from './Intelligence';
 
